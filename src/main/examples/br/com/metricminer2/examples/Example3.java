716c0f2a829b1630323fb31edf896f3eb7dec90e
Maurício Aniche <mauricioaniche@gmail.com>
2014-11-18 01:37:26 -0200

    an example with two processors at the same time

---
+++ src/main/examples/br/com/metricminer2/examples/Example3.java
@@ -0,0 +1,25 @@
+package br.com.metricminer2.examples;
+
+import br.com.metricminer2.SourceCodeRepositorySearch;
+import br.com.metricminer2.persistence.csv.CSVWriter;
+import br.com.metricminer2.scm.git.GitRepository;
+import br.com.metricminer2.scm.metrics.cc.MethodLevelCyclomaticComplexity;
+import br.com.metricminer2.scm.metrics.loc.ClassLevelLinesOfCode;
+import br.com.metricminer2.scm.processor.ClassLevelMetricProcessor;
+import br.com.metricminer2.scm.processor.MethodLevelMetricProcessor;
+
+public class Example3 {
+
+
+	public static void main(String[] args) {
+		String repoPath = Example1.class.getResource("/repo-1/").getPath();
+		String csvPath = Example1.class.getResource("/").getPath() + "cc.csv";
+		String locPath = Example1.class.getResource("/").getPath() + "loc.csv";
+		
+		new SourceCodeRepositorySearch()
+			.in(GitRepository.build(repoPath))
+			.process(new MethodLevelMetricProcessor(new MethodLevelCyclomaticComplexity()), new CSVWriter(csvPath))
+			.process(new ClassLevelMetricProcessor(new ClassLevelLinesOfCode()), new CSVWriter(locPath))
+			.start();
+	}
+}
