ea28247ad9a8a51e9b45ddb216c36f9c5d5ae268
Maurício Aniche <mauricioaniche@gmail.com>
2014-11-18 00:31:06 -0200

    method level class processor

---
+++ src/main/examples/br/com/metricminer2/examples/Example2.java
@@ -0,0 +1,24 @@
+package br.com.metricminer2.examples;
+
+import java.util.Arrays;
+
+import br.com.metricminer2.SourceCodeRepositorySearch;
+import br.com.metricminer2.output.csv.CSVWriter;
+import br.com.metricminer2.scm.metrics.cc.MethodLevelCyclomaticComplexity;
+import br.com.metricminer2.scm.processor.MethodLevelMetricProcessor;
+
+public class Example2 {
+
+
+	public static void main(String[] args) {
+		String repoPath = Example1.class.getResource("/repo-1/").getPath();
+		String csvPath = Example1.class.getResource("/").getPath() + "cc.csv";
+		
+		new SourceCodeRepositorySearch()
+			.in(Arrays.asList(repoPath))
+			.process(
+					new MethodLevelMetricProcessor(new MethodLevelCyclomaticComplexity()), new CSVWriter(csvPath)
+			)
+			.start();
+	}
+}
---
+++ src/main/java/br/com/metricminer2/scm/processor/MethodLevelMetricProcessor.java
@@ -0,0 +1,41 @@
+package br.com.metricminer2.scm.processor;
+
+import java.util.Map;
+
+import br.com.metricminer2.output.PersistenceMechanism;
+import br.com.metricminer2.scm.Commit;
+import br.com.metricminer2.scm.Modification;
+import br.com.metricminer2.scm.SCMProcessor;
+import br.com.metricminer2.scm.SCMRepository;
+import br.com.metricminer2.scm.metrics.MethodLevelCodeMetric;
+
+public class MethodLevelMetricProcessor implements SCMProcessor {
+
+	private MethodLevelCodeMetric metric;
+
+	public MethodLevelMetricProcessor(MethodLevelCodeMetric metric) {
+		this.metric = metric;
+	}
+
+	@Override
+	public void process(SCMRepository repo, Commit commit, PersistenceMechanism writer) {
+		
+		for(Modification change : commit.getModifications()) {
+			if(metric.accepts(change.getNewPath())) {
+				Map<String, Double> values = metric.calculate(change.getSourceCode());
+				for(Map.Entry<String, Double> entry : values.entrySet()) {
+					String method = entry.getKey();
+					Double value = entry.getValue();
+					writer.write(commit.getHash(), change.getNewPath(), method, value);
+				}
+			}
+		}
+
+	}
+
+	@Override
+	public String name() {
+		return "Method Level metric Processor for " + metric.getName();
+	}
+
+}
