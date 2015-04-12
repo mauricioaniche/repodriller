562673e7a32be162758cdb0eb6266985b8c85e75
Maurício Aniche <mauricioaniche@gmail.com>
2015-04-12 12:29:54 -0300

    mexendo no visitor de acordo com a sugestao do Rodrigo Pinho

--- src/main/java/br/com/metricminer2/metric/java8/Java8AntLRVisitor.java
+++ src/main/java/br/com/metricminer2/metric/java8/Java8AntLRVisitor.java
@@ -19,25 +19,49 @@ package br.com.metricminer2.metric.java8;
 import java.io.InputStream;
 
 import org.antlr.v4.runtime.ANTLRInputStream;
+import org.antlr.v4.runtime.BailErrorStrategy;
 import org.antlr.v4.runtime.CharStream;
 import org.antlr.v4.runtime.CommonTokenStream;
-import org.antlr.v4.runtime.tree.ParseTreeWalker;
+import org.antlr.v4.runtime.ConsoleErrorListener;
+import org.antlr.v4.runtime.DefaultErrorStrategy;
+import org.antlr.v4.runtime.RecognitionException;
+import org.antlr.v4.runtime.atn.PredictionMode;
+import org.antlr.v4.runtime.tree.*;
 
 import br.com.metricminer2.metric.java8.Java8Parser.CompilationUnitContext;
  
 public class Java8AntLRVisitor {
- 
+
 	public void visit(Java8Listener visitor, InputStream is) {
 		try {
 			CharStream input = new ANTLRInputStream(is);
 			Java8Lexer lex = new Java8Lexer(input);
 			CommonTokenStream tokens = new CommonTokenStream(lex);
 			Java8Parser parser = new Java8Parser(tokens);
-//			parser.getInterpreter().setPredictionMode(PredictionMode.SLL);
-			CompilationUnitContext compilationUnitContext = parser.compilationUnit();
-			new ParseTreeWalker().walk(visitor, compilationUnitContext);
+			parser.getInterpreter().setPredictionMode(PredictionMode.SLL);
+			parser.removeErrorListeners();
+			parser.setErrorHandler(new BailErrorStrategy());
+			try {
+				CompilationUnitContext compilationUnitContext = parser
+						.compilationUnit();
+				new ParseTreeWalker().walk(visitor, compilationUnitContext);
+			} catch (RuntimeException ex) {
+
+					tokens.reset(); // rewind input stream
+					
+					// back to standard listeners/handlers
+					parser.addErrorListener(ConsoleErrorListener.INSTANCE);
+					parser.setErrorHandler(new DefaultErrorStrategy());
+					parser.getInterpreter()
+							.setPredictionMode(PredictionMode.LL);
+					CompilationUnitContext compilationUnitContext = parser
+							.compilationUnit();
+					new ParseTreeWalker().walk(visitor, compilationUnitContext);
+			}
+
 		} catch (Exception e) {
 			throw new RuntimeException(e);
 		}
+
 	}
 }
