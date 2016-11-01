3846d91c8c6dffbcea0e30019598074e43c895f9
Maurício Aniche <mauricioaniche@gmail.com>
2016-11-01 12:03:11 +0100

    one more test to the diff parser (relates to #31)

--- src/test/java/org/repodriller/domain/DiffParserTest.java
+++ src/test/java/org/repodriller/domain/DiffParserTest.java
@@ -93,4 +93,53 @@ public class DiffParserTest {
 		Assert.assertTrue(newLines.contains(new DiffLine(7, "ee")));
 		Assert.assertTrue(newLines.contains(new DiffLine(8, "ff")));
 	}
+
+	@Test
+	public void additionsAndDeletions() {
+		
+		String diff =
+				"diff --git a/A b/A\r\n"+
+				"index 4624afb..870742d 100644\r\n"+
+				"--- a/A\r\n"+
+				"+++ b/A\r\n"+
+				"@@ -1,10 +1,10 @@\r\n"+
+				" aa\r\n"+
+				" aaa\r\n"+
+				"+xxx\r\n"+
+				" bb\r\n"+
+				" cc\r\n"+
+				" log.info(\"aa\")\r\n"+
+				" log.debug(\"b\")\r\n"+
+				" dd\r\n"+
+				" ee\r\n"+
+				"-log.trace()\r\n"+
+				" ff";
+		
+		DiffParser parsedDiff = new DiffParser(diff);
+		
+		List<DiffLine> oldLines = parsedDiff.getLinesInOldFile();
+		Assert.assertTrue(oldLines.contains(new DiffLine(1, "aa")));
+		Assert.assertTrue(oldLines.contains(new DiffLine(2, "aaa")));
+		Assert.assertTrue(oldLines.contains(new DiffLine(3, "bb")));
+		Assert.assertTrue(oldLines.contains(new DiffLine(4, "cc")));
+		Assert.assertTrue(oldLines.contains(new DiffLine(5, "log.info(\"aa\")")));
+		Assert.assertTrue(oldLines.contains(new DiffLine(6, "log.debug(\"b\")")));
+		Assert.assertTrue(oldLines.contains(new DiffLine(7, "dd")));
+		Assert.assertTrue(oldLines.contains(new DiffLine(8, "ee")));
+		Assert.assertTrue(oldLines.contains(new DiffLine(9, "log.trace()")));
+		Assert.assertTrue(oldLines.contains(new DiffLine(10, "ff")));
+		
+		List<DiffLine> newLines = parsedDiff.getLinesInNewFile();
+		Assert.assertTrue(newLines.contains(new DiffLine(1, "aa")));
+		Assert.assertTrue(newLines.contains(new DiffLine(2, "aaa")));
+		Assert.assertTrue(newLines.contains(new DiffLine(3, "xxx")));
+		Assert.assertTrue(newLines.contains(new DiffLine(4, "bb")));
+		Assert.assertTrue(newLines.contains(new DiffLine(5, "cc")));
+		Assert.assertTrue(newLines.contains(new DiffLine(6, "log.info(\"aa\")")));
+		Assert.assertTrue(newLines.contains(new DiffLine(7, "log.debug(\"b\")")));
+		Assert.assertTrue(newLines.contains(new DiffLine(8, "dd")));
+		Assert.assertTrue(newLines.contains(new DiffLine(9, "ee")));
+		Assert.assertTrue(newLines.contains(new DiffLine(10, "ff")));
+
+	}
 }
