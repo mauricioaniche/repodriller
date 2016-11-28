f885e0ed9d03f9f1426b4f173e06d3228a90b6d7
Maurício Aniche <mauricioaniche@gmail.com>
2016-11-28 20:12:22 +0100

    removing useless imports

--- src/test/java/org/repodriller/integration/BetweenDatesTest.java
+++ src/test/java/org/repodriller/integration/BetweenDatesTest.java
@@ -4,7 +4,6 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
-import java.util.GregorianCalendar;
 import java.util.TimeZone;
 
 import org.junit.Assert;
