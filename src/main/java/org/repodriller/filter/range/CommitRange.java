0ec563d11ceed5cf65776d501fe08904c72a9533
Jamie Davis <davisjam@vt.edu>
2017-10-05 13:21:31 -0400

    Some Javadoc. Towards #70.

--- src/main/java/org/repodriller/filter/range/CommitRange.java
+++ src/main/java/org/repodriller/filter/range/CommitRange.java
@@ -5,6 +5,20 @@ import java.util.List;
 import org.repodriller.domain.ChangeSet;
 import org.repodriller.scm.SCM;
 
+/**
+ * Return a set of commits from an SCM.
+ *
+ * @author Mauricio Aniche
+ */
+/* TODO It's confusing that this interface is called CommitRange but it returns a list of ChangeSet's, not Commits. */
 public interface CommitRange {
+
+	/**
+	 * Extract the desired commits from this SCM.
+	 *
+	 * @param scm	The SCM to probe.
+	 * @return	List of the ChangeSet's in the range.
+	 */
 	List<ChangeSet> get(SCM scm);
+
 }
