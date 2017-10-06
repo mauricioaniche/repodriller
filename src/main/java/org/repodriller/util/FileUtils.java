d6b113063c3d35cdc5d9ef9331803903065f366f
Jamie Davis <davisjam@vt.edu>
2017-10-06 09:58:32 -0400

    Javadoc for FileUtils

--- src/main/java/org/repodriller/util/FileUtils.java
+++ src/main/java/org/repodriller/util/FileUtils.java
@@ -7,8 +7,19 @@ import java.util.List;
 
 import org.apache.commons.io.IOUtils;
 
+/**
+ * Various utilities for working with files.
+ *
+ * @author Mauricio Aniche
+ */
 public class FileUtils {
 
+	/**
+	 * Get the absolute path to all subdirs of {@code path}.
+	 *
+	 * @param path	Root of directory tree
+	 * @return	All children of this root that are subdirs
+	 */
 	public static List<String> getAllDirsIn(String path) {
 		File dir = new File(path);
 		String[] files = dir.list();
@@ -20,13 +31,30 @@ public class FileUtils {
 				projects.add(possibleDir.getAbsolutePath());
 			}
 		}
-
 		return projects;
 	}
+
+	/**
+	 * Recursively find all the files in this tree, except those in .git and .svn subdirs
+	 *
+	 * @param pathToLook	Tree root
+	 * @return	Every file in this tree for which File.isFile(), except those in
+	 */
 	public static List<File> getAllFilesInPath(String pathToLook) {
 		return getAllFilesInPath(pathToLook, new ArrayList<>());
 	}
 
+	/**
+	 * Helper for getAllFilesInPath.
+	 *
+	 * @param pathToLook	Tree root
+	 * @param files	Ongoing collection of files (not dirs)
+	 * @return	{@code files} plus all files in the tree rooted at pathToLook
+	 *          Files that are in .svn or .git dirs are ignored.
+	 */
+	/* TODO It's strange that a FileUtils class knows abuot .svn and .git.
+	 * 		If you want to blacklist subtrees, these should be arguments, not hardcoded in this class.
+	 */
 	private static List<File> getAllFilesInPath(String pathToLook, List<File> files) {
 		for (File f : new File(pathToLook).listFiles()) {
 			if (f.isFile())
@@ -36,8 +64,13 @@ public class FileUtils {
 		}
 		return files;
 	}
-	
 
+	/**
+	 * Return the raw contents of this file
+	 *
+	 * @param f	File to read
+	 * @return	Contents of file as string
+	 */
 	public static String readFile(File f) {
 		try {
 			FileInputStream input = new FileInputStream(f);
@@ -45,11 +78,16 @@ public class FileUtils {
 			input.close();
 			return text;
 		} catch (Exception e) {
-			throw new RuntimeException("error reading file " + f.getAbsolutePath(), e);
+			throw new RuntimeException("Error reading file " + f.getAbsolutePath(), e);
 		}
 	}
-	
 
+	/**
+	 * True if f is a directory and doesn't look like svn or git metadata
+	 *
+	 * @param f
+	 * @return True if it looks like a "project subdirectory"
+	 */
 	private static boolean isAProjectSubdirectory(File f) {
 		return f.isDirectory() && !f.getName().equals(".svn") && !f.getName().equals(".git");
 	}
