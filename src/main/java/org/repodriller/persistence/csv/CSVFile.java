96eeb4f8bfa8b38a6903b2d11c4fad403faa49fc
Jamie Davis <davisjam@vt.edu>
2017-10-13 15:07:30 -0400

    CSVFile: Discuss thread safety, misc improvements

--- src/main/java/org/repodriller/persistence/csv/CSVFile.java
+++ src/main/java/org/repodriller/persistence/csv/CSVFile.java
@@ -16,95 +16,157 @@
 
 package org.repodriller.persistence.csv;
 
-import java.io.File;
 import java.io.FileOutputStream;
 import java.io.PrintStream;
-import java.util.Arrays;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
 
 import org.apache.commons.lang3.StringEscapeUtils;
-
+import org.apache.log4j.Logger;
+import org.repodriller.RepoDrillerException;
 import org.repodriller.persistence.PersistenceMechanism;
 
+/**
+ * A CSVFile
+ *
+ * @author Mauricio Aniche
+ */
 public class CSVFile implements PersistenceMechanism {
 
 	private PrintStream ps;
 
 	private String[] header = null;
-	
-	
+
+	private boolean writable = false;
+
+	private static final Logger log = Logger.getLogger(CSVFile.class);
+
+	/* Various constructors. */
+
+	/**
+	 * A CSVFile named fileName, overwrite existing contents, no header.
+	 *
+	 * @param fileName
+	 */
 	public CSVFile(String fileName) {
-		this(fileName, false);
+		this(fileName, false, null);
 	}
 
+	/**
+	 * A CSVFile named fileName, append?, no header.
+	 *
+	 * @param fileName
+	 * @param append If true we append, else we overwrite
+	 */
 	public CSVFile(String fileName, boolean append) {
-		try {
-			ps = new PrintStream(new FileOutputStream(fileName, append));
-		} catch (Exception e) {
-			throw new RuntimeException(e);
+		this(fileName, append, null);
+	}
+
+	/**
+	 * A CSVFile named fileName, append?, print this header?
+	 *
+	 * @param fileName
+	 * @param append If true we append, else we overwrite
+	 * @param header Print if not null
+	 */
+	public CSVFile(String fileName, boolean append, String[] header){
+		open(fileName, append);
+
+		/* Emit the header. */
+		this.header = header;
+		if (header != null)	{
+			log.error("Writing header");
+			try {
+				write((Object[]) header);
+			} catch (Exception e) {
+				throw new RuntimeException(e);
+			}
 		}
 	}
-	
+
+	/* Additional weird constructors. */
+
 	public CSVFile(String path, String name) {
 		this(path, name, false);
 	}
 
-	public CSVFile(String path, String name, boolean append) {
-		this(verifyPath(path) + name, append);
-	}		
-	
-	public CSVFile (String path, String name , String [] header) {
-		this(verifyPath(path) + name, header);
-	}
-	
-	public CSVFile (String fileName, String [] header) {
-		this(fileName,header,false);
+	public CSVFile (String fileName, String[] header) {
+		this(fileName, false, header);
 	}
 
-	public CSVFile(String fileName, String[] header, boolean append ){
-		this.header = header;
+	public CSVFile(String path, String name, boolean append) {
+		this(Paths.get(path, name).toAbsolutePath().toString(), append, null);
+	}
+
+	public CSVFile (String path, String name, String[] header) {
+		this(Paths.get(path, name).toAbsolutePath().toString(), false, header);
+	}
+
+	public CSVFile(String path, String name, boolean append, String[] header){
+		this(Paths.get(path, name).toAbsolutePath().toString(), append, header);
+	}
+
+	@Deprecated
+	public CSVFile(String fileName, String[] header, boolean append){
+		this(fileName, append, header);
+	}
+
+	/* Interface PersistenceMechanism. */
+
+	/**
+	 * Write a CSV record consisting of these fields.
+	 *
+	 * @arg fields	Objects with toString()'s. As many fields as are in the header, if you provided one
+	 */
+	@Override
+	public synchronized void write(Object... fields) throws CSVFileFormatException {
+		if (!writable)
+			throw new RepoDrillerException("Error, writing to a closed CSVFile");
+
+		if (header != null && header.length != fields.length)
+			throw new CSVFileFormatException("CSV header had " + header.length + " columns but you provided " + fields.length + " fields");
+
+		/* Convert fields to CSV-friendly strings. */
+		String[] escapedFields = Stream.of(fields).map(field -> {
+			if (field == null)
+				return "null";
+			else {
+				String raw = field.toString();
+				if (raw == null)
+					return "null";
+				return StringEscapeUtils.escapeCsv(raw);
+			}
+		})
+		.toArray(String[]::new);
+
+		/* Out it goes. */
+		String csvRecord = String.join(",", escapedFields);
+		log.debug("Writing csvRecord: " + csvRecord);
+		ps.println(csvRecord);
+
+		ps.flush(); /* TODO This has performance implications. */
+	}
+
+	@Override
+	public synchronized void close() {
+		ps.close();
+		writable = false;
+	}
+
+	/* Misc. helpers. */
+
+	/**
+	 * Open this CSVFile for business.
+	 *
+	 * @param fileName
+	 * @param append
+	 */
+	private void open(String fileName, boolean append) {
 		try {
 			ps = new PrintStream(new FileOutputStream(fileName, append));
-			if (header != null)	{
-				String headers = Arrays.toString(header);
-				headers = headers.substring(1, headers.length()-1);
-				ps.println(headers);	
-			}
+			writable = true;
 		} catch (Exception e) {
 			throw new RuntimeException(e);
 		}
-	}
-	
-	@Override
-	public synchronized void write(Object... line) throws CSVFileFormatException {
-		if (header != null && header.length != line.length)
-			throw new CSVFileFormatException("CSV Header Columns Number Differs From Writer Columns Number.");
-		
-		boolean first = true;
-		for(Object o : line) {
-			if(!first) ps.print(",");
-
-			if(o == null) ps.print("null");
-			else {
-				String field = o.toString();
-				field = StringEscapeUtils.escapeCsv(field);
-				ps.print(field);
-			}
-			first=false;
-		}
-		
-		ps.println();
-		ps.flush();
-
-	}
-	
-	public void close() {
-		ps.close();
-	}
-	
-	private static String verifyPath(String path) {
-		char lastchar = path.charAt(path.length()-1);
-		if (lastchar != File.separatorChar)
-			path = path + File.separator;
-		return path;		
 	}
 }
