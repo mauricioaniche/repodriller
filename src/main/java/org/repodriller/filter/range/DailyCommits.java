7c1a9f482a70e7d3b30e82b504c8c137fc0afa1e
Jamie Davis <davisjam@vt.edu>
2017-10-17 18:36:14 -0400

    Add a DailyCommits filter

---
+++ src/main/java/org/repodriller/filter/range/DailyCommits.java
@@ -0,0 +1,46 @@
+package org.repodriller.filter.range;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import org.apache.log4j.Logger;
+import org.repodriller.domain.ChangeSet;
+import org.repodriller.scm.SCM;
+
+public class DailyCommits implements CommitRange {
+
+	private final long daysInMillis;
+
+	private static final Logger log = Logger.getLogger(DailyCommits.class);
+
+	public DailyCommits(int days) {
+		daysInMillis = 1000L * 60L * 60L * 24L * days;
+	}
+
+	@Override
+	public List<ChangeSet> get(SCM scm) {
+
+		List<ChangeSet> all = scm.getChangeSets();
+
+		LinkedList<ChangeSet> filtered = new LinkedList<ChangeSet>();
+		filtered.add(all.get(0));
+
+		for(ChangeSet cs : all) {
+			if(isFarFromTheLastOne(cs, filtered)) {
+				filtered.addLast(cs);
+			}
+		}
+
+		return filtered;
+	}
+
+	private boolean isFarFromTheLastOne(ChangeSet cs, LinkedList<ChangeSet> filtered) {
+		ChangeSet lastOne = filtered.getLast();
+
+		long lastInMillis = lastOne.getTime().getTimeInMillis();
+		long currentInMillis = cs.getTime().getTimeInMillis();
+
+		return (lastInMillis - currentInMillis >= daysInMillis);
+	}
+
+}
