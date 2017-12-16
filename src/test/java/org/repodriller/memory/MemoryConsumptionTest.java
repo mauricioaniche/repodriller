9a8f77df9c1317294152912e25a47bd267685fd2
Mauricio Aniche <mauricioaniche@gmail.com>
2017-12-16 21:29:09 -0200

    moving to other package

--- src/test/java/org/repodriller/memory/MemoryConsumptionTest.java
+++ src/test/java/org/repodriller/memory/MemoryConsumptionTest.java
@@ -1,14 +1,11 @@
-package memory;
+package org.repodriller.memory;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
-import org.apache.http.NameValuePair;
 import org.apache.http.client.HttpClient;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.HttpClients;
-import org.apache.http.message.BasicNameValuePair;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.junit.Test;
