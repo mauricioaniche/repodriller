14068758ef2995c64cf7800de94b793cd28cec96
Mauricio Aniche <mauricioaniche@gmail.com>
2018-02-18 23:03:28 +0100

    path might be null, so PathUtils now can handle it

--- src/main/java/org/repodriller/util/PathUtils.java
+++ src/main/java/org/repodriller/util/PathUtils.java
@@ -3,6 +3,8 @@ package org.repodriller.util;
 public class PathUtils {
 
     public static String fullPath(String path) {
+        if(path==null) return null;
+
         if(path.startsWith("~"))
             path = path.replaceFirst("^~",System.getProperty("user.home"));
 
